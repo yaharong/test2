802d93000a87e16145b76520c9e7be54637a18dd
yaharong <bandinyou@naver.com>
2017-08-01 11:36:04 +0900

    커밋 커밋

---
+++ app/src/main/java/com/example/tacademy/testgit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tacademy.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 20170801 작업시작
+    }
+}
